7c34481f77e31cfa005f60a4e6b788159c371523
nico_lera17 <nleralopez@gmail.com>
2018-10-09 18:07:44 +0200

    Test 1 acabado y bien

--- tests/geometry_test.cpp
+++ tests/geometry_test.cpp
@@ -190,18 +190,19 @@ const lest::test TESTP[] =
                     Vec inCenter2 = translation2 * r2.getOrigin();
                     Vec inUCS2 = Matrix(planet2RS) * inCenter2; // PUNTO EN UCS
 
-
+                    r2.setOrigin(inUCS2);
+                    r1.setOrigin(inUCS1);
                     float distance = calculateDistances(r1,r2);
 
                     Vec v1 = calculateVector(r1,inUCS2);
 
                     Vec v2 = calculateVector(r2,inUCS1);
 
-                    float diff = abs(distance - 34.0);
+                    float diff = abs(distance - 39.0);
 
-                    float diff1 = abs(v1.getZ() + v1.getY() + v1.getZ() - 34.0);
+                    float diff1 = abs(v1.getX() + v1.getY() + v1.getZ() - 39.0);
 
-                    float diff2 = abs(v2.getZ() + v2.getY() + v2.getZ() - 34.0);
+                    float diff2 = abs(v2.getX() + v2.getY() + v2.getZ() - 39.0);
 
                     EXPECT( 0.001 > diff );
 
@@ -210,6 +211,90 @@ const lest::test TESTP[] =
                     EXPECT( 0.001 > diff2 );
 
                 },
+
+                CASE( "Test 1: \"Catapulta tiene que atravesar planetas\" ejecutado correctamente" )
+                {
+
+                    ReferenceSystem UCS(Vec(1,0,0, DIRECTION),
+                                        Vec(0,1,0,DIRECTION),
+                                        Vec(0,0,1,DIRECTION),
+                                        Vec(0,0,0,POINT));
+
+                    // First planet
+                    Vec axis1(0, 10, 0, DIRECTION);
+                    Vec referenceCity1(3.535533905932738+1, 3.535533905932738, 0, POINT); // The radius is 5
+                    Vec center1(1, 0, 0, POINT);
+                    Planet planet1(center1, axis1, referenceCity1);
+
+                    // First ReferenceSystem
+                    //ReferenceSystem r1(M_PI, M_PI_2, planet1);
+                    ReferenceSystem r1(M_PI_2, 0, planet1.getR(),planet1.getRadio());
+
+
+                    ReferenceSystem planet1RS = planet1.getR();
+
+                    Vec center1Changed = planet1.getCenter();
+                    center1Changed.changeSign();
+                    Matrix translation(UCS);
+                    translation.translation(center1Changed);
+
+                    Vec inCenter = translation * r1.getOrigin();
+                    Vec inUCS1 = Matrix(planet1RS) * inCenter; // PUNTO EN UCS
+
+                    // Second planet
+                    //Vec axis2(0, 10, 0, DIRECTION);
+                    Vec axis2(7.07106781187, 7.07106781187, 0, DIRECTION);
+                    //Vec axis2(0, 10, 0, DIRECTION);
+                    //Vec referenceCity2(-15, -10, 0, POINT);
+                    Vec referenceCity2(-3.535533905932738+50, 3.535533905932738, 0, POINT);
+                    Vec center2(50, 0, 0, POINT);
+                    Planet planet2(center2, axis2, referenceCity2);
+
+                    /*
+                    // Second ReferenceSystem
+                    ReferenceSystem r2(M_PI_2, 0, planet2.getR(),planet2.getRadio());
+
+                    Vec axis2(0, 10, 0, DIRECTION);
+                    Vec referenceCity2(3.535533905932738-50, 3.535533905932738+5, 0, POINT);
+                    Vec center2(-50, 5, 0, POINT);
+                    Planet planet2(center2, axis2, referenceCity2);
+
+                     */
+                    // Second ReferenceSystem
+                    ReferenceSystem r2(3.0*M_PI_4, 0, planet2.getR(),planet2.getRadio());
+
+                    ReferenceSystem planet2RS = planet2.getR();
+
+                    Vec center2Changed = planet2.getCenter();
+                    center2Changed.changeSign();
+                    Matrix translation2(UCS);
+                    translation2.translation(center2Changed);
+
+                    Vec inCenter2 = translation2 * r2.getOrigin();
+                    Vec inUCS2 = Matrix(planet2RS) * inCenter2; // PUNTO EN UCS
+
+                    r2.setOrigin(inUCS2);
+                    r1.setOrigin(inUCS1);
+                    float distance = calculateDistances(r1,r2);
+
+                    Vec v1 = calculateVector(r1,inUCS2);
+
+                    Vec v2 = calculateVector(r2,inUCS1);
+
+                    float diff = abs(distance - 39.0);
+
+                    float diff1 = abs(v1.getX() + v1.getY() + v1.getZ() - 39.0);
+
+                    float diff2 = abs(v2.getX() + v2.getY() + v2.getZ() - 39.0);
+
+                            EXPECT( 0.001 > diff );
+
+                            EXPECT( 0.001 > diff1 );
+
+                            EXPECT( 0.001 > diff2 );
+
+                }
+
         };
 
 int main( int argc, char * argv[] )
